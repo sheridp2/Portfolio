6246ef46a3e4cee4da5e2868a123a9e323070512
Patrick Sheridan <sheridp2@gmail.com>
2017-03-24 09:06:14 -0700

    server.js updated, works correctly

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./public'));
 
-app.get('/', function(request, response){
+app.get('*', function(request, response){
   response.sendFile('public/index.html', {root:'.'})
 });
 
 app.listen(PORT, function() {
-  console.log('Your app is being logged on localhost:3000');
+  console.log('localHost:',PORT);
 });
